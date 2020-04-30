b551e8e763985bab54028b6517ab9ab4498c110c
vince1016 <45361827+vince1016@users.noreply.github.com>
2020-04-29 20:40:12 -0400

    MakeViewDetail component added
    view should not have logic->compinents
    added

---
+++ tp2/src/components/MakeViewDetail.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h1>{{ movie.title }}</h1>
+        <img :src= movie.image alt="Affiche du film">
+        <p>{{ }}vote: (pas coder) etoile grise si pas de 5 crit</p>
+        <p>{{ movie.description }}</p>
+        <p>{{ movie.rating }}</p>
+        <p>{{ movie.length }} minutes</p>
+        <p>{{ movie.release_year }}</p>
+        <h3>Acteurs:</h3>
+            <div id="actors"  v-for="actor in actors" :key="actor.id" > 
+                <hr>
+                <p>Nom: {{actor.last_name}}</p>
+                <p>Prenom: {{actor.first_name}}</p>
+                <p>Date de naissance: {{actor.birthdate}}</p>
+            </div>
+    </div>
+</template>
+
+<script>
+import MoviesServices1 from '../services/MoviesService.js'
+
+    export default {
+         data() {
+            return {
+                error: null,
+                movie: null,
+                actors: null,
+            }
+        },
+        created () {
+                MoviesServices1.getMovie(this.$route.params.id)
+                .then(reponse =>{ 
+                    this.movie = reponse.data.film;
+                     console.log(reponse.data.film);
+                })
+                
+                .catch(error => {
+                    this.error = error;
+                })
+                MoviesServices1.getMovieActors(this.$route.params.id)
+                .then(reponse =>{
+                    this.actors = reponse.data;
+                })
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
