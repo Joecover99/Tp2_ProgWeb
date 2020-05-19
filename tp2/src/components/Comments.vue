cf25d067a064724f3f752e00795fcea14f5e6654
vince1016 <45361827+vince1016@users.noreply.github.com>
2020-05-19 17:39:57 -0400

    login made
    logique working
    need to link to admin page when auth

--- tp2/src/components/Comments.vue
+++ tp2/src/components/Comments.vue
@@ -50,7 +50,7 @@
          },
         
         methods:{
-            addComment: function (){
+            addComment(){
                 if(this.critics.id && this.critics.comment){
                     this.critics.comments.push({author: this.critics.critic_first_name + this.crtitics.critic_last_name, content: this.critics.comment})
                 }else{
