e5606cc1ed782035443cf7cf8a34dd2b2708e397
Joecover99 <34388196+Joecover99@users.noreply.github.com>
2020-05-24 21:24:21 -0400

    cote Membre

--- tp2/src/components/Carousel.vue
+++ tp2/src/components/Carousel.vue
@@ -7,7 +7,6 @@
             <div class="movieRating" >
                 <star-rating :rating="overallStarRating" :read-only="true" v-bind:increment="0.5"></star-rating>
             </div>  
-            <p><strong>overall Rating: </strong>{{overAllRating}}/ 100</p>
             <p> {{ sliceThis(movie.description) }} <strong v-if="movie.description.length > 100" > (...) </strong></p>
             <button @click="onSelect(movie)"><strong>details</strong></button>
         </div>
@@ -17,6 +16,7 @@
 <script>
 import { VueAgile } from 'vue-agile'
 import StarRating from 'vue-star-rating'
+
 export default {    
     components: {
         StarRating,
@@ -29,32 +29,6 @@ export default {
                 default: true
         }
     },
-    computed:{ 
-       /* overAllRating(){
-            if(this.critics.length < 5){
-                return "N/D";
-            }
-            else{
-                let overallScore = 0;
-                this.critics.forEach(c => overallScore += Number(c.score));
-                return (overallScore /= this.critics.length).toPrecision(3);
-            }
-        },
-        overallStarRating(){
-                if(this.critics.length < 5){
-                    return 0;
-                }
-                else{
-                    let overallScore = 0;
-                    let dividedScoreBy = 20;
-                    this.critics.forEach(c => overallScore += Number(c.score));
-                    overallScore = (overallScore /= this.critics.length).toPrecision(3);
-                    overallScore /=  dividedScoreBy;
-                    overallScore = Math.round(2*overallScore) / 2                  
-                    return overallScore;
-                }
-            },*/
-        }, 
     methods: {
 
         sliceThis(discription){
@@ -71,7 +45,6 @@ export default {
                  this.$router.push({ name: "movie", params: { id: movie.id} });
             }               
         },
-
     },
 }
 </script>
